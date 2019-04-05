fcdde6017e30365574663027da09df150ca33fd7
mojtaba <mojtaba_cs@yahoo.com>
2019-04-05 15:48:15 +0430

    Fix class PhysicalNewspaperSubscription to show address correctly

--- question4/PhysicalNewspaperSubscription.java
+++ question4/PhysicalNewspaperSubscription.java
@@ -4,20 +4,21 @@ public class PhysicalNewspaperSubscription extends NewspaperSubscription {
 
     @Override
     public void setAddress(String address) {
-        Boolean bool = false;
-        for (int i=0 ; i<=9 ; i++){
-            for (int j =0 ; j<address.length() ; j++){
-                if (address.charAt(j)== (char) i)
-                    bool=true;
-            }
-        }
 
-        if(bool) {
+        Boolean bool = false;
+
+        for (int i = 0; i <= 9; i++) {
+                if (address.contains(Integer.toString(i)))
+                    bool = true;
+            }
+
+
+        if (bool) {
             super.address = address;
             super.rate = 15.0;
-        }else {
+        } else {
             System.out.println("Please enter valid address!");
-            super.rate=0.0;
+            super.rate = 0.0;
         }
 
     }

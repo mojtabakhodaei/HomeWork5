b79ccaee50952452d447b459116b585a7e9e8025
mojtaba <mojtaba_cs@yahoo.com>
2019-04-05 12:29:55 +0430

    Create class IncomingPhoneCall

---
+++ question3/IncomingPhoneCall.java
@@ -0,0 +1,28 @@
+package HomeWork5.question3;
+
+public class IncomingPhoneCall extends PhoneCall {
+
+    public IncomingPhoneCall(String phoneNumber){
+        super.phoneNumber=phoneNumber;
+    }
+
+    @Override
+    public void setPrice() {
+        this.price = 0.02;
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    @Override
+    public Double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public void displayInformation() {
+        System.out.println("phone number: " + this.phoneNumber + " , rate: 0.02 , price: " + this.price);
+    }
+}

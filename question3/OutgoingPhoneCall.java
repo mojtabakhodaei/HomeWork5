726013dbe00c5c51af79d3ee5777de68d93fa81e
mojtaba <mojtaba_cs@yahoo.com>
2019-04-05 12:30:42 +0430

    Create class OutgoingPhoneCall

---
+++ question3/OutgoingPhoneCall.java
@@ -0,0 +1,31 @@
+package HomeWork5.question3;
+
+public class OutgoingPhoneCall extends PhoneCall {
+
+    Double callTime;
+
+    public OutgoingPhoneCall(String phonenumber , Double callTime){
+        super.phoneNumber=phonenumber;
+        this.callTime=callTime;
+    }
+
+    @Override
+    public void setPrice() {
+        this.price = 0.04*callTime;
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    @Override
+    public Double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public void displayInformation() {
+        System.out.println("phone number: " + this.phoneNumber + " , rate: 0.04" + " , call time: " + callTime + " , price: " + this.price);
+    }
+}

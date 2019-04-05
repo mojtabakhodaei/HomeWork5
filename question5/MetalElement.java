2a1a94f9ca2b88cb4f4e3bdb0e7882d6989575fd
mojtaba <mojtaba_cs@yahoo.com>
2019-04-05 16:36:53 +0430

    Reformat codes

--- question5/MetalElement.java
+++ question5/MetalElement.java
@@ -2,13 +2,13 @@ package HomeWork5.question5;
 
 public class MetalElement extends Element {
 
-    public MetalElement(String symbol, int atomicNumber, double atomicWeight){
-        super(symbol,atomicNumber,atomicWeight);
+    public MetalElement(String symbol, int atomicNumber, double atomicWeight) {
+        super(symbol, atomicNumber, atomicWeight);
     }
 
     @Override
-    public void describeElement(){
-        System.out.println("\nElement: "+ super.symbol + "\natomicNumber: " + super.atomicNumber+ "\natomicWeight: " + super.atomicWeight);
+    public void describeElement() {
+        System.out.println("\nElement: " + super.symbol + "\natomicNumber: " + super.atomicNumber + "\natomicWeight: " + super.atomicWeight);
         System.out.println("This Element is good conductor of electricity");
     }
 }

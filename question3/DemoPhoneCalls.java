e177ac1a15bff1a806b59208a10eaebfce07a78c
mojtaba <mojtaba_cs@yahoo.com>
2019-04-05 12:36:33 +0430

    create class DemoPhoneCalls

---
+++ question3/DemoPhoneCalls.java
@@ -0,0 +1,12 @@
+package HomeWork5.question3;
+
+public class DemoPhoneCalls {
+    public static void main(String[] args) {
+        PhoneCall p1 = new IncomingPhoneCall("09123456789");
+        PhoneCall p2 = new OutgoingPhoneCall("09352431901" , 3.5);
+
+        p1.displayInformation();
+        p2.displayInformation();
+    }
+
+}
